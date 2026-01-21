b3f7452454db65566a16a7149bb32e061c66ad47
Krishanu <k@ksankar.me>
2026-01-20 16:53:25 -0800

    attempted refactoring but ran into an issue about access to variables

--- src/wavesim.ts
+++ src/wavesim.ts
@@ -35,6 +35,7 @@ class State {
   // Adding any boundary conditions which override the differential equation
   add_boundary_conditions(arr: Array<number>, t: number): void {}
   // Step the differential equation forward
+  // TODO This part has to be moved outside, to the generic "Scene" class.
   step(dt: number) {
     return this.step_rk(dt);
   }
